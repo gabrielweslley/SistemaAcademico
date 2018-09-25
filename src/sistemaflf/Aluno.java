4349b1f2cf77b3fe9469fe8c09c9815bce58c120
Gabriel Weslley <39171347+gabrielweslley@users.noreply.github.com>
2018-09-25 20:54:59 -0300

    defeitos do codigo java
    mostrando alguns defeitos que podem ser melhorados no codigo java

--- src/sistemaflf/Aluno.java
+++ src/sistemaflf/Aluno.java
@@ -13,20 +13,22 @@ public class Aluno {
     private String telefone; 
     private String situação; 
     private String curso; 
+//  Aarray nao esta sendo usado  dentro do codigo
     ArrayList<String> disciplinas = new ArrayList<String>();
 
     
     // CONSTRUTOR DA CLASSE ALUNO 
 
     public Aluno(String nome, String cpf, String matricula, String endereço, String email, String telefone, String situação, String curso) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.matricula = matricula;
-        this.endereço = endereço;
-        this.email = email;
-        this.telefone = telefone;
-        this.situação = situação;
-        this.curso = curso;
+    //os construtores estao fazendo a mesma coisa do metodoss setters
+    //    this.nome = nome;
+    //  this.cpf = cpf;
+    //    this.matricula = matricula;
+    //    this.endereço = endereço;
+    //    this.email = email;
+    //    this.telefone = telefone;
+    //    this.situação = situação;
+    //    this.curso = curso;
     }
   
     
@@ -66,12 +68,12 @@ public class Aluno {
     
     
    
-    
-    public void statusAluno(){
-        System.out.println("Nome do aluno: "+ this.nome);
-        System.out.println("Matricula: "+ this.matricula);
-        System.out.println("CPF: " + this.cpf);
-        System.out.println("Endereço: " + this.endereço);
+   //ma pratica de programacao (nao se cria interacao dentro de metodos , tem que ser tudo no main) 
+    //public void statusAluno(){
+    //    System.out.println("Nome do aluno: "+ this.nome);
+    //    System.out.println("Matricula: "+ this.matricula);
+    //    System.out.println("CPF: " + this.cpf);
+    //    System.out.println("Endereço: " + this.endereço);
     }
     
 }
