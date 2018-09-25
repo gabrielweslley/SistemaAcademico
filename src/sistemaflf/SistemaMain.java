f63db2e4e28d3292ee48b48605199a6a1592850a
Gabriel Weslley <39171347+gabrielweslley@users.noreply.github.com>
2018-09-25 20:56:39 -0300

    melhorias
    melhorias codigo java

--- src/sistemaflf/SistemaMain.java
+++ src/sistemaflf/SistemaMain.java
@@ -6,6 +6,7 @@ public class SistemaMain {
     public static void main(String[] args){
      
         Aluno aluno = new Aluno("ggg","ggg","ggg","ggg","ggg","ggg","ggg","ggg"); 
+    // nao se cria metodo para enviar interacao com o usuario.
         aluno.statusAluno();
     
     
